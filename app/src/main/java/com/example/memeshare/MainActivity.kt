41179f4e84bb51711d076dbcd20f227a87ebbde4
mundhe27 <ashish02.mundhe@gmail.com>
2022-12-13 23:45:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/memeshare/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.memeshare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sharememe(view: View) {
+
+    }
+    fun nextmeme(view: View) {
+
+    }
+}
